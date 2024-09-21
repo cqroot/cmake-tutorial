aff589aaa5f8ee8a17c037ea1365d87a92913cb3
Keith Chu <cqroot@outlook.com>
2024-09-22 01:17:14 +0800

    feat: 添加 `02 - 新增一个 library`

---
+++ 02-adding_a_library/math/mysqrt.h
@@ -0,0 +1,6 @@
+#ifndef __MYSQRT_H
+#define __MYSQRT_H
+
+double mysqrt(double x);
+
+#endif /* ifndef __MYSQRT_H */
---
+++ 02-adding_a_library/tutorial.c
@@ -0,0 +1,8 @@
+#include "mysqrt.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("mysqrt(4) = %lf\n", mysqrt(4));
+    return 0;
+}
